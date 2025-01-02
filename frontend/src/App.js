8920ecd12048b0e77b2ef553978a86cca8f20e5a
osmancano <canohibro@gmail.com>
2025-01-02 13:32:26 +0300

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import { useState } from "react";
 
 const UNSPLASH_URL = "https://api.unsplash.com/photos/random/";
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
-//DEBUG = bool(os.environ.get("DEBUG", True));
 
 const App = () => {
-  const [word,setWord] = useState('');
+  const [word, setWord] = useState("");
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
@@ -20,16 +19,16 @@ const App = () => {
       })
       .catch((err) => {
         console.log(err);
-      })
-    setWord('');
-  }
+      });
+    setWord("");
+  };
 
   return (
     <div>
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit= {handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
