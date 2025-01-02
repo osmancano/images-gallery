ae7e71525e85e8c055cc223077dc48b292dcf54b
osmancano <canohibro@gmail.com>
2025-01-02 16:27:29 +0300

    Welcome container

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Welcome = () => (
+  <div style={{ backgroundColor: 'lightGray' }}>
+    <h1> Images Gallery</h1>
+    <p>
+      This is simple application that retrieve photos using Unsplash API. In
+      order to start enter any search in the input field.
+    </p>
+    <p>
+      <Button variant="primary" href="https://unsplash.com" target="_blank">
+        Learn more
+      </Button>
+    </p>
+  </div>
+);
+
+export default Welcome;
